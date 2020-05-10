0c1680c41a728e5fee1b6660fa124bb2c6c82086
Tony <mo3g87@mail.ru>
2020-05-10 23:19:24 +0300

    finished game

--- guess.go
+++ guess.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,5 +17,33 @@ func main() {
 	target := rand.Intn(100) + 1
 	fmt.Println("I've chosen random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left")
+
+		fmt.Println("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if guess < target {
+			fmt.Println("Ouch, your guess was too LOW.")
+		} else if guess > target {
+			fmt.Println("Your guess was too HIGH.")
+		} else {
+			fmt.Println("Well done, Turner!")
+			break
+		}
+	}
+	if !success {
+		fmt.Println("No Jerry, you fucked! Correct number was: ", target)
+	}
 }
