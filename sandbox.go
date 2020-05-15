2bc722e4ed0722c61137ccdff136b44ba5e90724
Tony <mo3g87@mail.ru>
2020-05-15 23:10:00 +0300

    added several return statements

--- sandbox.go
+++ sandbox.go
@@ -2,14 +2,29 @@ package main
 
 import "fmt"
 
-func paintNeeded(width float64, height float64) {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return width, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return height, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	fmt.Printf("%.2f shit is needed\n", area/10)
+	return area / 10.0, nil
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
-	paintNeeded(5.2, 3.5)
-	paintNeeded(5.0, 3.3)
+	var amount, total float64
+	amount, err := paintNeeded(4.2, -3.0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%0.2f shit needed\n", amount)
+	}
+	total += amount
+	amount, err = paintNeeded(5.2, 3.5)
+	fmt.Printf("%0.2f shit is needed\n", amount)
+	total += amount
+	fmt.Printf("Total: %0.2f shit \n", total)
 
 }
