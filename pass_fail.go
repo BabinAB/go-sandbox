ac308a3790b9743f6c3b1cff701cf3d20550f88c
Tony <mo3g87@mail.ru>
2020-05-16 22:56:02 +0300

    refactored pass_fail.go

--- pass_fail.go
+++ pass_fail.go
@@ -9,20 +9,26 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Print("Enter your grade: ")
+func getFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
-
 	input = strings.TrimSpace(input)
-	grade, err := strconv.ParseFloat(input, 64)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+
+func main() {
+	fmt.Print("Enter your grade: ")
+	grade, err := getFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	var status string
 	if grade >= 60 {
 		status = "passed"
